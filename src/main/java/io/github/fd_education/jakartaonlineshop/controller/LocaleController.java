79eb69c4f7b2e26ce58c9b72e5ac251d02b43e31
Fabian Diemand <fabian.diemand@students.ffhs.ch>
2022-11-23 00:35:49 +0100

    feature(registration): adjust basic ui to how it is modeled

---
+++ src/main/java/io/github/fd_education/jakartaonlineshop/controller/LocaleController.java
@@ -0,0 +1,27 @@
+package io.github.fd_education.jakartaonlineshop.controller;
+
+import java.io.Serializable;
+
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+
+@Named
+@SessionScoped
+public class LocaleController implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String lang = "de_DE";
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    public String change(String lang) {
+        this.lang = lang;
+        return "/index.jsf";
+    }
+}
