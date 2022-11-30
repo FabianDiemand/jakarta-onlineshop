c201964eeb7f5e4f9903db9c237b5432e8a3f95a
Fabian Diemand <fabian.diemand@students.ffhs.ch>
2022-11-30 20:08:05 +0100

    feature(registration): validate registration in email

--- src/main/java/io/github/fd_education/jakartaonlineshop/controller/RegisterController.java
+++ src/main/java/io/github/fd_education/jakartaonlineshop/controller/RegisterController.java
@@ -4,12 +4,17 @@ import io.github.fd_education.jakartaonlineshop.ejb.register.RegisterBeanLocal;
 import jakarta.ejb.EJB;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.faces.application.FacesMessage;
+import jakarta.faces.component.UIComponent;
 import jakarta.faces.context.FacesContext;
+import jakarta.faces.validator.ValidatorException;
 import jakarta.inject.Named;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.regex.Pattern;
 
 @Named
 @RequestScoped
@@ -41,5 +46,22 @@ public class RegisterController implements Serializable {
         }
 
         return "/register.jsf";
+    }
+
+    public void isEmail(FacesContext fc, UIComponent uic, Object obj) throws ValidatorException {
+        String value = (String) obj;
+
+        Locale locale = fc.getViewRoot().getLocale();
+        ResourceBundle bundle = ResourceBundle.getBundle("messages", locale);
+
+        if(value.isEmpty()){
+            FacesMessage fm = new FacesMessage(bundle.getString("email_empty"));
+            throw new ValidatorException(fm);
+        }
+
+        if(!Pattern.matches("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$", value)){
+            FacesMessage fm = new FacesMessage(bundle.getString("email_invalid"));
+            throw new ValidatorException(fm);
+        }
     }
 }
