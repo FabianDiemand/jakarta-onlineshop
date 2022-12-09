ceea6a8374556567278a11ca48db93491cd16c8e
Fabian Diemand <fabian.diemand@students.ffhs.ch>
2022-12-09 09:43:16 +0100

    feature(orders): finish nav styling for profile

--- src/main/java/io/github/fd_education/jakartaonlineshop/controller/OrderController.java
+++ src/main/java/io/github/fd_education/jakartaonlineshop/controller/OrderController.java
@@ -1,7 +1,10 @@
 package io.github.fd_education.jakartaonlineshop.controller;
 
+import io.github.fd_education.jakartaonlineshop.model.entities.Customer;
+import io.github.fd_education.jakartaonlineshop.model.entities.Order;
 import io.github.fd_education.jakartaonlineshop.model.entities.Product;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
 import java.util.Set;
@@ -9,7 +12,10 @@ import java.util.Set;
 @Named @RequestScoped
 public class OrderController {
 
-    public void createOrderFromCart(Set<Product> cart){
+    @Inject
+    Order order;
+
+    public void createOrderFromCart(Customer customer, Set<Product> cart){
 
     }
 }
